63fdd069320eeb0d4eb3d1e46f7b0a298c3760ea
Gianni <giannisalvatore4@gmail.com>
2022-11-23 11:16:14 +0100

    colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import Seo from "../components/seo";
 
-import '../styles/reset.css'
-import '../styles/globals.css'
+import "../styles/reset.css";
+import "../styles/globals.css";
+import "../styles/colors.css";
 
 function MyApp({ Component, pageProps }) {
-    return <>
-        <Seo />
-        <Component {...pageProps} />
-    </>
+    return (
+        <>
+            <Seo />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
