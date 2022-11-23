aaa49dece1a786b0c13ef209ac670f771ed7ad33
Gianni <giannisalvatore4@gmail.com>
2022-11-23 12:24:42 +0100

    dinamic import components

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,50 @@
-import Navigation from "../components/navigation";
-import Hero from "../components/hero";
-import Work from "../components/work";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const Navigation = dynamic(() => import("../components/navigation"), {
+    suspense: true,
+});
+const Hero = dynamic(() => import("../components/hero"), {
+    suspense: true,
+});
+const Work = dynamic(() => import("../components/work"), {
+    suspense: true,
+});
 
 export default function Home() {
     const works = getWorks();
-    return (<>
-        <Navigation />
-        <Hero />
-        <Work data={works.shampora} />
-        <Work data={works.shampora2} />
-    </>)
+    return (
+        <Suspense fallback={`Loading...`}>
+            <Navigation />
+            <Hero />
+            <Work data={works.shampora} />
+            <Work data={works.shampora2} />
+        </Suspense>
+    );
 }
+
 function getWorks() {
     return {
         shampora: {
             title: "Shampora",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non consectetur nunc. Vivamus at orci in ligula volutpat mattis. Ut auctor lectus pulvinar, porta erat tristique, suscipit metus. Morbi non egestas neque, sed hendrerit turpis. Pellentesque eu felis et nisl dapibus consectetur sit amet vel augue. Cras at nibh at magna laoreet varius. Suspendisse pretium malesuada tincidunt. Nulla feugiat, turpis vitae tempus lacinia.",
-            image: "/images/work_shampora.jpg"
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non consectetur nunc. Vivamus at orci in ligula volutpat mattis. Ut auctor lectus pulvinar, porta erat tristique, suscipit metus. Morbi non egestas neque, sed hendrerit turpis. Pellentesque eu felis et nisl dapibus consectetur sit amet vel augue. Cras at nibh at magna laoreet varius. Suspendisse pretium malesuada tincidunt. Nulla feugiat, turpis vitae tempus lacinia.",
+            image: "/images/work_shampora.jpg",
+            job: {
+                title: "Full stack developer",
+                date: "March 2018 - Present",
+                tasks: [
+                    "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+                    "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify",
+                    "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+                ],
+            },
         },
         shampora2: {
             title: "Shampora",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non consectetur nunc. Vivamus at orci in ligula volutpat mattis. Ut auctor lectus pulvinar, porta erat tristique, suscipit metus. Morbi non egestas neque, sed hendrerit turpis. Pellentesque eu felis et nisl dapibus consectetur sit amet vel augue. Cras at nibh at magna laoreet varius. Suspendisse pretium malesuada tincidunt. Nulla feugiat, turpis vitae tempus lacinia.",
-            image: "/images/work_shampora.jpg"
-        }
-    }
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non consectetur nunc. Vivamus at orci in ligula volutpat mattis. Ut auctor lectus pulvinar, porta erat tristique, suscipit metus. Morbi non egestas neque, sed hendrerit turpis. Pellentesque eu felis et nisl dapibus consectetur sit amet vel augue. Cras at nibh at magna laoreet varius. Suspendisse pretium malesuada tincidunt. Nulla feugiat, turpis vitae tempus lacinia.",
+            image: "/images/work_shampora.jpg",
+        },
+    };
 }
