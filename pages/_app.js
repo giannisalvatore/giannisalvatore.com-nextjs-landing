a9a326b4324949d1fdc85c584b474523c59446d8
Gianni <giannisalvatore4@gmail.com>
2022-11-23 11:20:30 +0100

    rename css with consistent name

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import Seo from "../components/seo";
 
 import "../styles/reset.css";
-import "../styles/globals.css";
+import "../styles/fonts.css";
 import "../styles/colors.css";
+import "../styles/palette.css";
 
 function MyApp({ Component, pageProps }) {
     return (
